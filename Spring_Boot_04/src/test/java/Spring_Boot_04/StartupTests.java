12ee48b7df1b23deb8f28cef0ddb57e34840c64e
veloso <veloso.rr31@hotmail.com>
2026-09-10 14:30:38 -0400

    aula 4

---
+++ Spring_Boot_04/src/test/java/Spring_Boot_04/StartupTests.java
@@ -0,0 +1,13 @@
+package Spring_Boot_04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

129ce121a90840c48f7f55f220f53c258dfe569c
veloso <veloso.rr31@hotmail.com>
2026-09-12 17:15:25 -0400

    criação dos pacotes de Exception

---
+++ README.md
@@ -0,0 +1,4 @@
+# Spring_Boot
+# teste após executar 
+# No navegador
+# http://localhost:8080/greeting?name=veloso
---
+++ Spring_Boot_04/src/main/java/br/com/Spring_Boot_04/exception/hadler/CustomEntityResponseHandler.java
@@ -0,0 +1,9 @@
+package br.com.Spring_Boot_04.exception.hadler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice
+@RestController
+public class CustomEntityResponseHandler {
+}
